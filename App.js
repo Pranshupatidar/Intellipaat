ab52d3e7710bec10fb066870cb3f8b04f1816384
HellbenderDeveloper <62550565+Pranshupatidar@users.noreply.github.com>
2022-05-05 19:36:45 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return <SafeAreaView style={backgroundStyle}></SafeAreaView>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
