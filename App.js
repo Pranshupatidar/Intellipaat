2613bd4d59606b652e7870aeac28f33727497fd3
HellbenderDeveloper <62550565+Pranshupatidar@users.noreply.github.com>
2022-05-05 22:53:32 +0530

    working code

--- App.js
+++ App.js
@@ -8,9 +8,19 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return <SafeAreaView style={{flex: 1}}></SafeAreaView>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 const styles = StyleSheet.create({});
---
+++ src/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
